91ea2cc6e200a6d9b4539328ce28b3a50227c0b6
Max Jacobson <max@hardscrabble.net>
2017-03-25 23:53:00 -0400

    Extract user and tidy up
    Not sure if this was all worth doing

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,5 @@
 // TODO: remove duplication in client logic
-// TODO: organize library code into multiple files
 // TODO: move tests into their own tests directory
-
-extern crate hyper;
-extern crate hyper_native_tls;
 
 #[macro_use]
 extern crate error_chain;
@@ -11,98 +7,16 @@ extern crate error_chain;
 #[macro_use]
 extern crate serde_derive;
 
-extern crate serde_json;
-
-mod errors {
-    error_chain!{}
-}
-
-use errors::*;
-
-use std::io::Read;
-
+mod errors;
 mod etag;
-use etag::Etag;
-
 mod subscription;
-use subscription::Subscription;
-
 mod subscriptions;
-use subscriptions::Subscriptions;
-
-pub struct User {
-    email: String,
-    password: String,
-}
-
-impl User {
-    pub fn new(email: String, password: String) -> User {
-        User {
-            email: email,
-            password: password,
-        }
-    }
-
-    pub fn authenticated(&self) -> Result<bool> {
-        let ssl = hyper_native_tls::NativeTlsClient::new().
-            chain_err(|| "Unable to intialize ssl client")?;
-        let connector = hyper::net::HttpsConnector::new(ssl);
-        let client = hyper::Client::with_connector(connector);
-
-        let resp = client.get("https://api.feedbin.com/v2/authentication.json")
-            .headers(self.basic_auth_headers())
-            .send()
-            .chain_err(|| "Unable to request auth status")?;
-
-        Ok(resp.status == hyper::Ok)
-    }
-
-    // TODO: allow providing etag
-    // TODO: allow providing "since" value
-    pub fn subscriptions(&self) -> Result<Subscriptions> {
-        let ssl = hyper_native_tls::NativeTlsClient::new().
-            chain_err(|| "Unable to intialize ssl client")?;
-        let connector = hyper::net::HttpsConnector::new(ssl);
-        let client = hyper::Client::with_connector(connector);
-
-        let mut resp = client.get("https://api.feedbin.com/v2/subscriptions.json")
-            .headers(self.basic_auth_headers())
-            .send()
-            .chain_err(|| "Unable to request subscriptions")?;
-
-
-        let mut body = String::new();
-        resp.read_to_string(&mut body).chain_err(|| "Unable to read response")?;
-
-        let subscriptions: Vec<Subscription> =
-            serde_json::from_str(&body).chain_err(|| "Couldn't deserialize response")?;
-
-        // FIXME: no unwrap please
-        // This is an Option, not a Result, so how does that fit into the picture?
-        let etag: &Etag = resp.headers.get().unwrap();
-
-        Ok(Subscriptions {
-               list: subscriptions,
-               etag: etag.to_owned(),
-           })
-    }
-
-    fn basic_auth_headers(&self) -> hyper::header::Headers {
-        let mut headers = hyper::header::Headers::new();
-        headers.set(hyper::header::Authorization(hyper::header::Basic {
-                                                     username: self.email.to_owned(),
-                                                     password: Some(self.password.to_owned()),
-                                                 }));
-
-        headers
-    }
-}
-
+mod user;
 
 #[cfg(test)]
 mod tests {
     use std::env;
-    use User;
+    use user::User;
 
     #[test]
     fn authenticated() {
---
+++ src/user.rs
@@ -0,0 +1,76 @@
+use errors::*;
+use subscription::Subscription;
+use subscriptions::Subscriptions;
+use etag::Etag;
+extern crate hyper;
+extern crate hyper_native_tls;
+use std::io::Read;
+extern crate serde_json;
+
+pub struct User {
+    email: String,
+    password: String,
+}
+
+impl User {
+    pub fn new(email: String, password: String) -> User {
+        User {
+            email: email,
+            password: password,
+        }
+    }
+
+    pub fn authenticated(&self) -> Result<bool> {
+        let ssl = hyper_native_tls::NativeTlsClient::new().
+            chain_err(|| "Unable to intialize ssl client")?;
+        let connector = hyper::net::HttpsConnector::new(ssl);
+        let client = hyper::Client::with_connector(connector);
+
+        let resp = client.get("https://api.feedbin.com/v2/authentication.json")
+            .headers(self.basic_auth_headers())
+            .send()
+            .chain_err(|| "Unable to request auth status")?;
+
+        Ok(resp.status == hyper::Ok)
+    }
+
+    // TODO: allow providing etag
+    // TODO: allow providing "since" value
+    pub fn subscriptions(&self) -> Result<Subscriptions> {
+        let ssl = hyper_native_tls::NativeTlsClient::new().
+            chain_err(|| "Unable to intialize ssl client")?;
+        let connector = hyper::net::HttpsConnector::new(ssl);
+        let client = hyper::Client::with_connector(connector);
+
+        let mut resp = client.get("https://api.feedbin.com/v2/subscriptions.json")
+            .headers(self.basic_auth_headers())
+            .send()
+            .chain_err(|| "Unable to request subscriptions")?;
+
+
+        let mut body = String::new();
+        resp.read_to_string(&mut body).chain_err(|| "Unable to read response")?;
+
+        let subscriptions: Vec<Subscription> =
+            serde_json::from_str(&body).chain_err(|| "Couldn't deserialize response")?;
+
+        // FIXME: no unwrap please
+        // This is an Option, not a Result, so how does that fit into the picture?
+        let etag: &Etag = resp.headers.get().unwrap();
+
+        Ok(Subscriptions {
+               list: subscriptions,
+               etag: etag.to_owned(),
+           })
+    }
+
+    fn basic_auth_headers(&self) -> hyper::header::Headers {
+        let mut headers = hyper::header::Headers::new();
+        headers.set(hyper::header::Authorization(hyper::header::Basic {
+                                                     username: self.email.to_owned(),
+                                                     password: Some(self.password.to_owned()),
+                                                 }));
+
+        headers
+    }
+}
